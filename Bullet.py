9c0c8e42658e9e660b0344d201ceb51150be887d
mufeng <1433187543@qq.com>
2019-04-07 16:48:09 +0800

    '英雄子弹'

---
+++ Bullet.py
@@ -0,0 +1,16 @@
+from Tool import *
+class Bullet(pygame.sprite.Sprite):
+    '''创建玩家炮弹类'''
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bullet_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
