312cf72dfc2844e0c485a74986150cecb453233c
mufeng <1433187543@qq.com>
2019-04-07 16:49:51 +0800

    '导弹'

---
+++ Missile.py
@@ -0,0 +1,17 @@
+from Tool import *
+class Missile(pygame.sprite.Sprite):
+    '''创建导弹类'''
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = missile_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
+
